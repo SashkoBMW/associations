6d7b299730af931abd58813e82a441c14a755eb1
Saša Serafimović <124636671+SashkoBMW@users.noreply.github.com>
2023-03-20 14:30:44 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,70 @@
+function z() {
+	let a = document.getElementById('kolA');
+    
+	if((a.value==="HERAKLIT") || (a.value==="heraklit"))
+	{
+		document.getElementById('provA').innerHTML = 'Tacno';
+
+	}
+	else 
+	{
+		document.getElementById('provA').innerHTML  = 'Netacno';
+}
+};
+
+
+function r() {
+	let a = document.getElementById('kolB');
+    
+	if((a.value==="ZENON") || (a.value==="zenon"))
+	{
+		document.getElementById('provB').innerHTML = 'Tacno';
+
+	}
+	else 
+	{
+		document.getElementById('provB').innerHTML  = 'Netacno';
+}
+};
+
+function k() {
+	let a = document.getElementById('kon');
+    
+	if((a.value==="PREDSOKRATOVCI") || (a.value==="predsokratovci"))
+	{
+		document.getElementById('provK').innerHTML = 'Tacno';
+
+	}
+	else 
+	{
+		document.getElementById('provK').innerHTML  = 'Netacno';
+}
+};
+
+function l() {
+	let a = document.getElementById('kolV');
+    
+	if((a.value==="PROTAGORA") || (a.value==="protagora"))
+	{
+		document.getElementById('provV').innerHTML = 'Tacno';
+
+	}
+	else 
+	{
+		document.getElementById('provV').innerHTML  = 'Netacno';
+}
+};
+
+function m() {
+	let a = document.getElementById('kolG');
+    
+	if((a.value==="TALES") || (a.value==="tales"))
+	{
+		document.getElementById('provG').innerHTML = 'Tacno';
+
+	}
+	else 
+	{
+		document.getElementById('provG').innerHTML  = 'Netacno';
+}
+};
